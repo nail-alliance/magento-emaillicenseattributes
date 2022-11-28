b8ce81bd0c3c1e70a40565ea448055ecd623dd02
ralenaNA <ralena@nailalliance.com>
2022-11-28 13:50:45 -0600

    Init

---
+++ Setup/Patch/Data/AddLicenseAttributes.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Nailalliance\EmailLicenseAttributes\Setup\Patch\Data;
+
+use Magento\Catalog\Ui\DataProvider\Product\ProductCollectionFactory;
+use Magento\Customer\Model\Customer;
+use Magento\Eav\Model\Config;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class AddLicenseAttributes
+ */
+class AddLicenseAttributes implements DataPatchInterface, PatchRevertableInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+    /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+    /**
+     * @var ProductCollectionFactory
+     */
+    private $productCollectionFactory;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+    /**
+     * @var Config
+     */
+    private $eavConfig;
+    /**
+     * @var \Magento\Customer\Model\ResourceModel\Attribute
+     */
+    private $attributeResource;
+
+    /**
+     *  * @package Magenest\CustomerAttribute\Setup\Patch\Data
+     *    constructor.
+     * @param EavSetupFactory $eavSetupFactory
+     * @param Config $eavConfig
+     * @param LoggerInterface $logger
+     * @param \Magento\Customer\Model\ResourceModel\Attribute $attributeResource
+     */
+    public function __construct(
+        EavSetupFactory $eavSetupFactory,
+        Config $eavConfig,
+        LoggerInterface $logger,
+        \Magento\Customer\Model\ResourceModel\Attribute $attributeResource,
+        \Magento\Framework\Setup\ModuleDataSetupInterface $moduleDataSetup
+    ) {
+        $this->eavSetupFactory = $eavSetupFactory;
+        $this->eavConfig = $eavConfig;
+        $this->logger = $logger;
+        $this->attributeResource = $attributeResource;
+        $this->moduleDataSetup = $moduleDataSetup;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        $this->AddLicenseAttribute3();
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * @throws \Magento\Framework\Exception\AlreadyExistsException
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \Zend_Validate_Exception
+     */
+    public function AddLicenseAttribute3()
+    {
+        $eavSetup = $this->eavSetupFactory->create();
+
+
+        $eavSetup->addAttribute(
+            \Magento\Customer\Model\Customer::ENTITY,
+            'customer_will_email_license',
+            [
+                'type' => 'int',
+                'label' => 'Will Email Proof of License?',
+                'input' => 'boolean',
+                'required' => 0,
+                'visible' => 1,
+                'user_defined' => 1,
+                'sort_order' => 999,
+                'position' => 999,
+                'source' => 'Magento\Eav\Model\Entity\Attribute\Source\Boolean',
+                'system' => 0,
+                'backend' => ''
+            ]
+        );
+
+        $attributeSetId = $eavSetup->getDefaultAttributeSetId(Customer::ENTITY);
+        $attributeGroupId = $eavSetup->getDefaultAttributeGroupId(Customer::ENTITY);
+
+        $attribute = $this->eavConfig->getAttribute(Customer::ENTITY, 'customer_will_email_license');
+
+        $attribute->setData('attribute_set_id', $attributeSetId);
+        $attribute->setData('attribute_group_id', $attributeGroupId);
+
+        $attribute->setData('used_in_forms', [
+            'adminhtml_customer',
+            'customer_account_create',
+        ]);
+
+        $this->attributeResource->save($attribute);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     *
+     */
+    public function revert()
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © 2018 Rees Alena. All rights reserved.
+ * See COPYING.txt for license details.
+ *
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Nailalliance_EmailLicenseAttributes',
+    __DIR__
+);
